83086f0df84cc5fc8d1af6e4bda30d72c3761826
Pola ArtInt <54287155+PolaArtInt@users.noreply.github.com>
2021-09-16 19:01:35 +0400

    Day 5 Commit
    Task 1. Меню как закрыть, не понимаю. Кнопку в GamePage тоже похерила.
    Task 2. Done.
    Task 3.  Я вообще не понимаю, что делать. Покемонов перенесла просто на GamePage. Читаю задание, но смысл не понимаю, даже как помощь просить не знаю, потому что не могу сформулировать вопрос.((

--- src/routes/Game/index.js
+++ src/routes/Game/index.js
@@ -1,7 +1,13 @@
+import Layout from '../../components/LayoutsBlock/Layout.js';
+import PokemonCard from '../../components/CardBlock/PokemonCard.js';
+
+import POKEMONS from '../../data/POKEMONS.json';
+
 import style from './game.module.css';
+import style1 from '../Home/home.module.css';
 
 const GamePage = ({ onChangePage }) => {
-	
+	const appStyle = {style1};
 	const handleClickButton = (page) => {
 		onChangePage && onChangePage(page);
 	};
@@ -9,6 +15,29 @@ const GamePage = ({ onChangePage }) => {
 	return (
 		<div className={style.flex}>
 			<h1 className={style.header}>This is a Game Page!</h1>
+			
+			<Layout 
+				style={appStyle}
+				title="Flip the card"
+				colorBg="transparent"
+			>	
+				<div className={style1.flex}>
+					{
+						
+						POKEMONS.map((item, index) => 
+							<PokemonCard 
+								key={item.id}
+								name={item.name}
+								img={item.img}
+								id={item.id}
+								type={item.type} 
+								values={item.values}
+							/>
+							)
+					}
+				</div>
+			</Layout>
+
 			<button
 				onClick={handleClickButton}
 			>
--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,10 +1,8 @@
 import Header from '../../components/HeaderBlock/Header.js';
-import Footer from '../../components/FooterBlock/Footer.js';
 import Layout from '../../components/LayoutsBlock/Layout.js';
-import PokemonCard from '../../components/CardBlock/PokemonCard.js';
-import MenuHeader from '../../components/NavBlock/MenuHeader.js';
+// import PokemonCard from '../../components/CardBlock/PokemonCard.js';
 
-import POKEMONS from '../../data/POKEMONS.json';
+// import POKEMONS from '../../data/POKEMONS.json';
 
 import background1 from '../../img/bg-1.jpg';
 import background2 from '../../img/bg-2.jpg';
@@ -22,7 +20,6 @@ const HomePage = ({ onChangePage }) => {
 
   return (
     <>
-			<MenuHeader />
 			<Header 
 				style={appStyle}
 				title="The Pokémon Card Game"
@@ -37,39 +34,22 @@ const HomePage = ({ onChangePage }) => {
 				<p>To win, a majority of the total ten cards played (including the one card that is not placed on the board) must be of the player's card color. To do this, the player must capture cards by placing a card adjacent to an opponent's card whereupon the 'ranks' of the sides where the two cards touch will be compared. If the rank of the opponent's card is higher than the player's card, the player's card will be captured and turned into the opponent's color. If the player's rank is higher, the opponent's card will be captured and changed into the player's color instead.</p>
 				<p>In the game two players face off against one another, one side playing as "blue", the other as "red" on a 3x3 grid.
 				Each player has five cards in a hand and the aim is to capture the opponent's cards by turning them into the player's own color of red or blue.</p>
-				<div className={style.flex}>
+				{/* <div className={style.flex}>
 					<img src={cardsImg} alt="pokemon cards" className={style.flex_img}/>
-				</div>
+				</div> */}
 			</Layout>
 			
 			<Layout 
 				style={appStyle}
 				title="Flip the card"
 				colorBg="transparent"
-			>	
-			
-				<div className={style.flex}>
-					{
-						
-						POKEMONS.map((item, index) => 
-							<PokemonCard 
-								key={item.id}
-								name={item.name}
-								img={item.img}
-								id={item.id}
-								type={item.type} 
-								values={item.values}
-							/>
-							)
-					}
-				</div>
-			</Layout>
+			/>
+
 			<Layout 
 				style={appStyle}
 				title="This is title"
 				urlBg={ background2 }
 			/>
-			<Footer />
 		</>
   );
 }
